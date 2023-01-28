5b5cc0db76fd49707fc4474755c72885d675048d
DaniilElmirov <elmirovwork@gmail.com>
2023-01-28 22:17:43 +0700

    Добавлены сущности в domain слой

---
+++ app/src/main/java/ru/cft/shift2023winter/domain/entities/Location.kt
@@ -0,0 +1,11 @@
+package ru.cft.shift2023winter.domain.entities
+
+data class Location(
+    val id: Int,
+    val name: String,
+    val type: String,
+    val dimension: String,
+    val residents: List<String>,
+    val url: String,
+    val created: String,
+)
