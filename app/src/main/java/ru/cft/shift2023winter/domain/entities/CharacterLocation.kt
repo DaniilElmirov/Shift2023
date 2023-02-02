10b1169843afb6cadce08641a440b0e3549e91c4
DaniilElmirov <elmirovwork@gmail.com>
2023-02-02 15:52:02 +0700

    Внесены изменения в сущности для передачи между фрагментами

--- app/src/main/java/ru/cft/shift2023winter/domain/entities/CharacterLocation.kt
+++ app/src/main/java/ru/cft/shift2023winter/domain/entities/CharacterLocation.kt
@@ -1,6 +1,10 @@
 package ru.cft.shift2023winter.domain.entities
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class CharacterLocation(
     val name: String,
     val url: String,
-)
+) : Parcelable
